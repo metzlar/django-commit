9a3f3f813424e79c6a0a27f7032a2094ae865330
Ivan Metzlar <metzlar@gmail.com>
2013-11-14 14:02:55 -0800

    Forgot setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup # pragma: no cover 
+
+setup(
+  name='Django Commit',
+  version='0.1',
+  py_modules=['django_commit'],
+  cmdclass={'upload':lambda x:None},
+  install_requires=[
+    'django',
+    'pip'
+  ],
+)# pragma: no cover 
